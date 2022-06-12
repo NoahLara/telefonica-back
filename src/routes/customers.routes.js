8186d8350d92edc8265560da2c60db9d227572bb
Noah <noe.serrano@creativaconsultores.com>
2022-06-12 13:27:40 -0600

    Adding JsonWebToken to backend

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -3,11 +3,20 @@ import { getCustomers, createCustomer, getCustomer, deleteCustomer, updateCustom
 
 const router = Router();
 
+/**
+ * Customers
+ */
 router.get('/customers', getCustomers); // get All customers
 router.post('/customers', createCustomer); // save customer
 router.get('/customers/:Id', getCustomer); // get Only 1 customer
 router.put('/customers/:Id', updateCustomer); // update customer
 router.delete('/customers/:Id', deleteCustomer); // delete customer
+
+/**
+ * Users
+ */
+
+
 
 
 export default router;
